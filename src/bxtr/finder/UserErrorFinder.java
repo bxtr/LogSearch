f3af3d47e0e63333cd77d8e98f80f3395705e331
Egor <podolyak.egor@gmail.com>
2017-02-05 12:49:11 +0300

    Add search log of User

---
+++ src/bxtr/finder/UserErrorFinder.java
@@ -0,0 +1,56 @@
+package bxtr.finder;
+
+import bxtr.Main;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ * Created by egor on 05.02.2017.
+ */
+public class UserErrorFinder  implements Observer, ConsoleOutput {
+
+
+    private  Pattern BEGIN_MESSAGE_PATTERN = null;
+    private static final Pattern INTERMEDIATE_MESSAGE_PATTERN = Pattern.compile(".*ERROR.*");
+    private static final Pattern END_MESSAGE_PATTERN = Pattern.compile(".*(INFO|DEBUG).*");
+    private  String name;
+    private String line;
+    private int lineNumber;
+    private int currentLineIndex;
+    private boolean flag;
+    private Map<Integer, List<String>> map;
+
+
+    public UserErrorFinder(String name) {
+        this.name = name;
+        this.BEGIN_MESSAGE_PATTERN = Pattern.compile(".*"+name+".*");
+        flag = false;
+        map = new HashMap<>();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if(o instanceof SimpleReader) {
+            SimpleReader reader = (SimpleReader) o;
+            line = reader.getLine();
+            lineNumber = reader.getLineNumber();
+            if(BEGIN_MESSAGE_PATTERN.matcher(line).find() && INTERMEDIATE_MESSAGE_PATTERN.matcher(line).find()) {
+                flag = true;
+                currentLineIndex = reader.getLineNumber();
+                map.put(currentLineIndex, new ArrayList<>());
+            }
+            if(flag) {
+                map.get(currentLineIndex).add(line);
+            }
+            if(END_MESSAGE_PATTERN.matcher(line).find()) {
+                flag = false;
+            }
+        }
+    }
+
+    @Override
+    public void console() {
+        Main.printErrorMap(map);
+    }
+}
