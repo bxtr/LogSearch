2d82883382ff2f7957246d7e41ed575eb404020d
bxtr <bxtr@outlook.com>
2017-02-05 11:21:46 +0300

    New ErrorLogFinder implementation.

--- src/bxtr/finder/ErrorLogFinder.java
+++ src/bxtr/finder/ErrorLogFinder.java
@@ -1,23 +1,52 @@
 package bxtr.finder;
 
-import java.util.Observable;
-import java.util.Observer;
+import bxtr.Main;
+
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
  * Поисковик сообщений об ошибке.
  * Created by bxtr on 01.02.2017.
  */
-public class ErrorLogFinder implements Observer {
-    private static final String BEGIN_MESSAGE_PATTERN = ".*ERROR.*";
-    private static final String END_MESSAGE_PATTERN = ".*(INFO|DEBUG).*";
-/*
+public class ErrorLogFinder implements Observer, ConsoleOutput {
+    private static final Pattern BEGIN_MESSAGE_PATTERN = Pattern.compile(".*ERROR.*");
+    private static final Pattern END_MESSAGE_PATTERN = Pattern.compile(".*(INFO|DEBUG).*");
+
+    private String line;
+    private int lineNumber;
+    private int currentLineIndex;
+    private boolean flag;
+    private Map<Integer, List<String>> map;
+
     public ErrorLogFinder() {
-        super(Pattern.compile(BEGIN_MESSAGE_PATTERN), Pattern.compile(END_MESSAGE_PATTERN));
-    }*/
+        flag = false;
+        map = new HashMap<>();
+    }
 
     @Override
     public void update(Observable o, Object arg) {
+        if(o instanceof SimpleReader) {
+            SimpleReader reader = (SimpleReader) o;
+            line = reader.getLine();
+            lineNumber = reader.getLineNumber();
+            if(BEGIN_MESSAGE_PATTERN.matcher(line).find()) {
+                flag = true;
+                currentLineIndex = reader.getLineNumber();
+                map.put(currentLineIndex, new ArrayList<>());
+            }
+            if(flag) {
+                map.get(currentLineIndex).add(line);
+            }
+            if(END_MESSAGE_PATTERN.matcher(line).find()) {
+                flag = false;
+            }
 
+        }
+    }
+
+    @Override
+    public void console() {
+        Main.printErrorMap(map);
     }
 }
